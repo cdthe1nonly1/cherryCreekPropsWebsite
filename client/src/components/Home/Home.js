892a97b5a28773300b363d0feaf040b09a064164
Charles Dykstra <charles.b.dykstra@gmail.com>
2023-02-20 19:15:11 -0700

    added in src client and components folder and other basic file setup

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+import { Card, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Home() {
+    return(
+        
+    )
+}
